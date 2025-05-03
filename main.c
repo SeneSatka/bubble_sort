e5863f185345f19fefbc055c168e4bc0cff67edd
SeneSatka <senesatka@gmail.com>
2025-05-03 19:33:06 +0300

    Bubble sort

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#define ll long long
+int main(int, char**){
+    int n;
+    scanf("%d", &n);//Read count of number(n)
+    ll int a[n];//Create an array size of n
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);//Get array elements(numbers)
+    }
+    for(int i=0; i < n; i++){
+        for(int j=i+1; j < n; j++){
+            if(a[i] > a[j]){//Compare a and a+1. numbers and if a is greater than a+1 swap they.
+                ll int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+    for(int i=0; i < n; i++){
+        printf("%d ", a[i]);//Print sorted array
+    }
+    //Written by: SeneSatka
+}
