4d1ff379b8e7fa86ce09a6f054c7f6a0db7061eb
SeneSatka <senesatka@gmail.com>
2025-05-03 20:01:36 +0300

    fix type convertion bug

--- main.c
+++ main.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
 #define ll long long
-int main(int, char**){
+int main(int, char **)
+{
     int n;
-    scanf("%d", &n);//Read count of number(n)
-    ll int a[n];//Create an array size of n
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &a[i]);//Get array elements(numbers)
+    scanf("%d", &n); // Read count of number(n)
+    ll int a[n];     // Create an array size of n
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%lld", &a[i]); // Get array elements(numbers)
     }
-    for(int i=0; i < n; i++){
-        for(int j=i+1; j < n; j++){
-            if(a[i] > a[j]){//Compare a and a+1. numbers and if a is greater than a+1 swap they.
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (a[i] > a[j])
+            { // Compare a and a+1. numbers and if a is greater than a+1 swap they.
                 ll int temp = a[i];
                 a[i] = a[j];
                 a[j] = temp;
             }
         }
     }
-    for(int i=0; i < n; i++){
-        printf("%d ", a[i]);//Print sorted array
+    for (int i = 0; i < n; i++)
+    {
+        printf("%lld ", a[i]); // Print sorted array
     }
-    //Written by: SeneSatka
+    // Written by: SeneSatka
 }
